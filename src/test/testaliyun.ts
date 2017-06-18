30c1fd785e93c801e07a391a6f8b38d82e47e480
jiangwei <jiangwei@gagogroup.com>
2017-06-18 14:37:25 +0800

    test(alien getOssclient): configuration method unites

---
+++ src/test/testaliyun.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by jiangwei on 2017/6/18.
+ * Copyright (c) 2017 (jw872505975@gmail.com). All rights reserved.
+ */
+import * as chai from "chai";
+import {Aliyun, AliyunConfig} from "../service/aliyun";
+
+describe("Aliyun ", () => {
+  it("Test getOssClient()", () => {
+    let config: AliyunConfig = new AliyunConfig("123456", "test_accessKeyId", "test_accessKeySecret", "test_region");
+    Aliyun.setConfig(config);
+    let ossClient: any = Aliyun.getOssClient("gago-data");
+    chai.expect(ossClient["options"]["region"]).to.equal("test_region");
+    chai.expect(ossClient["options"]["accessKeyId"]).to.equal("test_accessKeyId");
+    chai.expect(ossClient["options"]["accessKeySecret"]).to.equal("test_accessKeySecret");
+  });
+});
