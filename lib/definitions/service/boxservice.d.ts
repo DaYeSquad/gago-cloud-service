8ef301a1b8bf21715183f5c9faba614ac00391ce
jiangwei <jiangwei@gagogroup.com>
2017-06-19 13:30:21 +0800

    chore(lib): delete lib

--- lib/definitions/service/boxservice.d.ts
+++
@@ -1,8 +0,0 @@
-import { OssFile, OssQuery, OssListOptions, OssSignatureUrlOptions } from '../module/ossfile';
-export declare class BoxService {
-    static uploadFileToOss(filePath: string, bucket: string, objectKey: string): Promise<number>;
-    static downloadFileToOss(bucket: string, objectKey: string, localPath: string): Promise<any>;
-    static getObjectUrl(bucket: string, objectKey: string, baseUrl?: string): Promise<string>;
-    static signatureUrl(bucket: string, objectKey: string, options?: OssSignatureUrlOptions): Promise<string>;
-    static list(bucket: string, query?: OssQuery, options?: OssListOptions): Promise<OssFile[]>;
-}
